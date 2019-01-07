1cd4e73bce8754db2cf8abe526b137dd367dec62
JoelAgot <joelagot@joelagot-X441SA>
2019-01-07 15:53:02 +0800

    assests

---
+++ assets/TopBar.php
@@ -0,0 +1,18 @@
+<!-- inserting Contact-->
+
+<div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
+  <div class="title-bar-title">PhoneBook</div>
+</div>
+
+<div class="top-bar" id="responsive-menu">
+  <div class="top-bar-left">
+    <ul class="menu" menu topbar-input-width='300'>
+    <li class="menu-text">PhoneBook</li>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><a type="button" href="index.php" class="button">Back</a></li>
+    </ul>
+  </div>
+</div>
